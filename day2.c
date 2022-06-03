c30ed23c90a0d27b3156c4c2915b36816b66a694
Sumeet Agarwal <sumeet.a@gmail.com>
2022-06-02 21:09:06 -0700

    finish day2b

--- day2.c
+++ day2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_PROGRAM_SIZE 80000
 
@@ -7,13 +8,29 @@ int run_program(int *program, int *pc);
 void parse_input(char *filename, int *program, int *pc);
 
 int main(void) {
-    int program[MAX_PROGRAM_SIZE]; // TODO: initialize these to be -1?
+    int parsed_program[MAX_PROGRAM_SIZE];
     int pc = 0;
-    parse_input("./day2.input", program, &pc);
+    parse_input("./day2.input", parsed_program, &pc);
+
+    int program[MAX_PROGRAM_SIZE];
+    memcpy(program, parsed_program, sizeof(parsed_program));
+    // part 1
     program[1] = 12;
     program[2] = 2;
-    run_program(program, &pc);
-    printf("%d\n", program[0]);
+    printf("part1: %d\n", run_program(program, &pc));
+
+    // part 2
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 100; j++) {
+            memcpy(program, parsed_program, sizeof(parsed_program));
+            program[1] = i;
+            program[2] = j;
+            if (run_program(program, &pc) == 19690720) {
+                printf("part2: %d\n", 100 * i + j);
+            }
+        }
+    }
+
     return 0;
 }
 
@@ -33,9 +50,10 @@ int run_program(int *program, int *pc) {
             *pc += 4;
             break;
         case 99:
+            *pc = 0;
             return program[0];
         default:
-            printf("invalid status code %d at PC %d\n", program[*pc], *pc);
+            printf("invalid opcode %d at PC %d\n", program[*pc], *pc);
             exit(1);
     }
 
@@ -45,7 +63,6 @@ int run_program(int *program, int *pc) {
 void parse_input(char *filename, int *program, int *pc) {
     FILE *file = fopen(filename, "r");
     int this_intcode = 0;
-
     for (char c = fgetc(file); c != EOF; c = fgetc(file)) {
         // there's a newline at the end of the file
         if (c == ',' || c == '\n') {
@@ -55,7 +72,6 @@ void parse_input(char *filename, int *program, int *pc) {
             this_intcode = (this_intcode * 10) + c - '0';
         }
     }
-
     *pc = 0;
 }
 
